61019053096d5d0d44ddc8cd3e72349d7ec1e44b
Robert <rquesadaqq@outlook.com>
2026-01-29 21:05:06 -0600

    Add mongoose info for data base

--- Workshop_1/index.js
+++ Workshop_1/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
 const app = express();
 
 app.use(express.json());
